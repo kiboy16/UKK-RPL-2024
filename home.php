3119c6071e2a64e12d79185c1e29fd41049d3a20
zacky saputra <160191856+kiboy16@users.noreply.github.com>
2024-02-16 15:10:14 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['userid'])){
+        header("location:login.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <p>Selamat Datang <b><?=$_SESSION['namalengkap']?></b></p>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="album.php">Album</a></li>
+        <li><a href="foto.php">Foto</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</body>
+</html>
